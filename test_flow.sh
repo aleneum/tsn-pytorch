45507bbf61d003b33d47b9bda15d9c301afebe0b
Alexander Neumann <aleneum@gmail.com>
2018-10-19 17:19:52 +0200

    add test/training scripts

---
+++ test_flow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# parameters: num_classes model num_gpu test_segments
+
+python /tsn_pytorch/test_models.py $1 Flow \
+    /generated/data/flow_val_split.txt \
+    /generated/models/$2 --arch BNInception -j $3 --test_segments $4 \
+    --save_scores /generated/$2_scores.npz
---
+++ train_flow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# parameters: num_class num_gpu batch_size
+
+python /tsn_pytorch/main.py $1 Flow \
+   /generated/data/flow_train_split.txt /generated/data/flow_val_split.txt \
+   --arch BNInception --num_segments 3 --gd 20 --lr 0.001 --lr_steps 190 300 --epochs 340 -b $3 -j $2 --dropout 0.7 \
+   --snapshot_pref /generated/models/tsn_flow --flow_pref flow_ 
