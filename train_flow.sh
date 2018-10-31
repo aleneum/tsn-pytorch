883a11acb3174ec6c5eda0ff48121fd87a030039
Alexander Neumann <aleneum@gmail.com>
2018-10-31 09:50:51 +0100

    add rest of arguments. save torch checkpoints with epoch number

--- train_flow.sh
+++ train_flow.sh
@@ -7,4 +7,4 @@ export TORCH_HOME='/tsn_pytorch'
 python /tsn_pytorch/main.py $1 Flow \
    /generated/data/flow_train_split.txt /generated/data/flow_val_split.txt \
    --arch BNInception --num_segments 3 --gd 20 --lr 0.001 --lr_steps 190 300 --epochs 340 -b $3 -j $2 --dropout 0.7 \
-   --snapshot_pref /generated/models/tsn_flow --flow_pref flow_ 
+   --snapshot_pref /generated/models/tsn_flow --flow_pref flow_ ${@:4}
