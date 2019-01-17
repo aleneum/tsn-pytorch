933901d131759662dca41c0b3e84861995bd60df
Alexander Neumann <aleneum@gmail.com>
2019-01-17 14:15:04 +0100

    add flow_prefix param

--- test_flow.sh
+++ test_flow.sh
@@ -7,4 +7,4 @@ export TORCH_HOME='/tsn_pytorch'
 python /tsn_pytorch/test_models.py $1 Flow \
     /generated/data/flow_val_split.txt \
     /generated/models/$2 --arch BNInception -j $3 --test_segments $4 \
-    --save_scores /generated/data/tsn_pytorch_flow_scores.npz ${@:5}
+    --save_scores /generated/data/tsn_pytorch_flow_scores.npz --flow_prefix flow_ ${@:5}
